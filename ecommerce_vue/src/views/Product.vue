bb844d0b709340f5b8de75cd429f812faced42f2
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-04-12 02:21:44 +0600

    bulma toast added

--- ecommerce_vue/src/views/Product.vue
+++ ecommerce_vue/src/views/Product.vue
@@ -28,6 +28,7 @@
 
 <script>
 import axios from 'axios'
+import { toast } from 'bulma-toast'
 
 export default {
     name: 'Product',
@@ -64,6 +65,15 @@ export default {
                 quantity: this.quantity
             }
             this.$store.commit('addToCart', item)
+
+            toast({
+                message: 'The product was added to the cart',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right'
+            })
         }
     }
 }
