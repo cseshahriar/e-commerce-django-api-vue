91ebbae2bc29e5ea24f32d347fced836dbc6621d
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-04-17 00:22:26 +0600

    order list

---
+++ ecommerce_vue/src/components/OrderSummery.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="box mb-4">
+        <h3 class="is-size-4 mb-6">Order #{{ order.id }}</h3>
+
+        <h4 class="is-size-5">Products</h4>
+
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <td>Product</td>
+                    <td>Price</td>
+                    <td>Quantity</td>
+                    <td>Total</td>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="item in order.items" v-bind:key="item.product.id">
+                    <td>{{ item.product.name }}</td>
+                    <td>{{ item.product.price }}</td>
+                    <td>{{ item.quantity }}</td>
+                    <td>${{ getItemTotal(item).toFixed(2) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OrderSummery',
+    props: {
+        order: Object
+    },
+    methods: {
+        getItemTotal(item) {
+            return item.quantity * item.product.price
+        },
+        orderTotalLength(order) {
+            return order.items.reduce( (acc, currentVal) => {
+                return acc += currentVal.quantity
+            }, 0)
+        }
+    }
+}
+</script>
--- ecommerce_vue/src/views/MyAccount.vue
+++ ecommerce_vue/src/views/MyAccount.vue
@@ -10,15 +10,38 @@
                 <button class="button is-danger" @click="logout()">Log out</button>
             </div>
 
+            <div class="column is-12">
+                <h2 class="subtitle">My orders</h2>
+                <!-- order summery component -->
+                <OrderSummery
+                v-for="order in orders"
+                v-bind:key="order.id"
+                v-bind:order="order"/>
+
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import OrderSummery from '@/components/OrderSummery.vue'
 import axios from 'axios'
 
 export default {
     name: 'MyAccount',
+    components:{
+        OrderSummery
+    },
+    data() {
+        return {
+            orders: []
+        }
+    },
+    mounted() {
+        document.title = 'My Account | Ecommerce Store'
+        this.getMyOrders()
+    }
+    ,
     methods: {
         logout() {
             axios.defaults.headers.common['Authorization'] = ''
@@ -28,6 +51,20 @@ export default {
 
             this.$store.commit('removeToken')
             this.$router.push('/')
+        },
+        async getMyOrders() {
+            this.$store.commit('setIsLoading', true)
+            
+            await axios
+                .get('api/v1/orders/list')
+                .then(response => {
+                    this.orders = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+            this.$store.commit('setIsLoading', false)
         }
     }
 }
